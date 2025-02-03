cbd531b1fa29b7c56adcedffd03f5ac9798b4cbc
e-lglioui <elgliouiyoucode@gmail.com>
2025-02-03 10:08:48 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
- 
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
