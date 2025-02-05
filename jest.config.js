3632f62322b10d665f51ccb238a61e4175875c77
e-lglioui <elgliouiyoucode@gmail.com>
2025-02-05 09:43:24 +0100

    fixing error in test stage

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: '.',
+      outputName: 'junit.xml',
+      classNameTemplate: '{classname}',
+      titleTemplate: '{title}',
+      ancestorSeparator: ' › ',
+      usePathForSuiteName: true
+    }]
+  ]
+}; 
